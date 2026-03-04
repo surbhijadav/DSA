8899fde576b2fdc387fd312bebd3ef8bcb2c7f79
SurbhiJadav <jadavsubhu04@outlook.com>
2026-03-04 22:30:30 +0530

    pair sum

---
+++ pairSum/optimalApproach.cpp
@@ -0,0 +1,34 @@
+// Pair Sum 
+// return Pair in sorted array with target sum.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> pairSum(vector<int> nums, int target) {
+    vector<int>  ans;
+    int n = nums.size();
+    int i = 0, j = n-1;
+
+    while(i<j){
+        int pairSum = nums[i] + nums[j];
+        if(pairSum > target) {
+            j--;
+        }else {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {2,7,11,15};
+    int target = 19;
+
+    vector<int> ans = pairSum(nums,target);
+    cout << ans[0] << "," << ans[1] << endl;
+
+    return 0;
+}
